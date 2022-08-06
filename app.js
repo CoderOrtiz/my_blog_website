6419e035740166a151a37fcec62cd709c908512a
Daniel Ortiz <ortiz.daniel1994@gmail.com>
2022-08-05 21:57:38 -0400

    Added Images, Updated Each Page with CSS and content

--- app.js
+++ app.js
@@ -28,10 +28,9 @@ const postSchema = new mongoose.Schema ({
 // Mongoose Model
 const Post = mongoose.model("Post", postSchema);
 
-// Declaring Content for Home, About, and Contact Pages
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+// Declaring Content for Home, and Contact Pages
+const homeStartingContent = "Welcome to my Blog Website! Here, you can write any blog post of your choosing by clicking, “COMPOSE” in the upper right-hand corner. Just enter a title of your post and whatever you want to discuss! If you want to know more about this project, feel free to stop by the About Section. If you want to get in contact with me, please visit the Contact Section. Thanks for visiting and please write a post just to mark that you were here and saw my Blog Website!";
+const contactContent = "Welcome to my Contact Page! The best way to get in contact with me is through LinkedIn! Stop by my GitHub if you have the time 🙂";
 
 
 ///////////////////////// Home Page /////////////////////////////
@@ -99,9 +98,8 @@ const requestedPostId = req.params.postId;
 
 // When in /about Renders About.EJS and its Content
 app.get("/about", function(req, res){
-  res.render("about", {aboutContent: aboutContent});
+  res.render("about");
 });
-
 
 ///////////////////////// Contact Page /////////////////////////////
 
